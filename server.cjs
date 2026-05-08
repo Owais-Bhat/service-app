d638c5c5363234efd04bbeeb95d55a5671cd8597
Owais Bhat <owais.bhat2612@gmail.com>
2026-05-08 09:52:10 +0530

    feat: migrate server to CommonJS and implement authentication and database API endpoints

---
+++ server.cjs
@@ -0,0 +1,2 @@
+// This file is the entry point for Hostinger deployment (CommonJS)
+require('./server/index.js');
--- server.js
+++
@@ -1,3 +0,0 @@
-// This file is the entry point for Hostinger deployment
-// It simply points to your actual server logic
-require('./server/index.js');
