a9017c48269686d53c2598c5833d14a179b208db
Owais-Bhat <owais.bhat2612@gmail.com>
2026-08-17 12:24:37 +0530

    feat(mobile): wire ThemeProvider and NEST font loading into App root

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import * as SplashScreen from 'expo-splash-screen';
 import { AuthProvider } from './src/context/AuthContext';
+import { ThemeProvider } from './src/theme/ThemeContext';
+import { useAppFonts } from './src/theme/fonts';
 import RootNavigator from './src/navigation/RootNavigator';
 
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+  const [fontsLoaded, fontError] = useAppFonts();
+
+  useEffect(() => {
+    if (fontsLoaded || fontError) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
     <SafeAreaProvider>
-      <AuthProvider>
-        <StatusBar style="light" />
-        <RootNavigator />
-      </AuthProvider>
+      <ThemeProvider>
+        <AuthProvider>
+          <StatusBar style="light" />
+          <RootNavigator />
+        </AuthProvider>
+      </ThemeProvider>
     </SafeAreaProvider>
   );
 }
